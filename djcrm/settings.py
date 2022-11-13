084c637f6564a9402022bf10770d3212b754e1ca
Sam Akan <samakan061@gmail.com>
2022-11-13 10:40:52 +0300

    Finishing Up

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -137,6 +137,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = "media_root"
+
 STATIC_ROOT = "static_root"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
